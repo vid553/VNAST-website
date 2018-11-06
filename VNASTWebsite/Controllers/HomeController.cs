c96bfb00d638898cb73645259b735d254d735378
Alen <alen.masel5@gmail.com>
2018-11-06 20:29:25 +0100

    Edited Home View

--- VNASTWebsite/Controllers/HomeController.cs
+++ VNASTWebsite/Controllers/HomeController.cs
@@ -7,30 +7,27 @@ using VNASTWebsite.Models;
 
 namespace VNASTWebsite.Controllers
 {
+    [HandleError]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             List<Assignment> list = new List<Assignment>();
-            list.Add(new Assignment("NAME1",DateTime.Parse("24.10.2018"),DateTime.Parse("30.11.2018"),"ACTIVE"));
+            list.Add(new Assignment("NAME1", DateTime.Parse("24.10.2018"), DateTime.Parse("30.11.2018"), "ACTIVE"));
             list.Add(new Assignment("NAME2", DateTime.Parse("25.10.2018"), DateTime.Parse("31.12.2018"), "ACTIVE"));
             list.Add(new Assignment("NAME3", DateTime.Parse("26.10.2018"), DateTime.Parse("15.12.2018"), "ACTIVE"));
-            list.Add(new Assignment("NAME4", DateTime.Parse("20.10.2018"), DateTime.Parse("21.12.2018"), "ACTIVE"));
-            list.Add(new Assignment("NAME5", DateTime.Parse("23.10.2018"), DateTime.Parse("15.1.2019"), "ACTIVE"));
+            list.Add(new Assignment("NAME4", DateTime.Parse("20.10.2018"), DateTime.Parse("21.12.2018"), "INACTIVE"));
+            list.Add(new Assignment("NAME5", DateTime.Parse("23.10.2018"), DateTime.Parse("15.1.2019"), "INACTIVE"));
             List<User> allUsers = new List<User>();
-            
+
             User currentUser = new User();
             User leaderUser = new User();
-            leaderUser.Username="John Doe";
+            leaderUser.Username = "John Doe";
             Group skupina = new Group();
             skupina.Leader = leaderUser;
             skupina.Name = "GROUP1";
             skupina.Workers = new List<User>();
-            skupina.Workers.Add(new User());
-            skupina.Workers.Add(new User());
-            skupina.Workers.Add(new User());
-            skupina.Workers.Add(new User());
-            skupina.Workers.Add(new User());
+            skupina.Workers.Add(currentUser);
             currentUser.Assignments = list;
             currentUser.Username = "WorkerUser1";
             currentUser.Role = "Worker";
@@ -42,10 +39,10 @@ namespace VNASTWebsite.Controllers
             {
                 return View(currentUser);
             }
-       /*     else if (currentUser.Role == "Manager")
+            else if (currentUser.Role == "Manager")
             {
-                return View(allUsers);
-            }*/
+                return View(currentUser);
+            }
             else
             {
                 return View(currentUser);
