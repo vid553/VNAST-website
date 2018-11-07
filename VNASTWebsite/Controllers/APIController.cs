65ec18fe05dacf8307b18bd5fbfcd665e546c9d8
Vid Rajtmajer <vid.rajtmajer@gmail.com>
2018-11-07 01:18:26 +0100

    Added API methods GET and POST

--- VNASTWebsite/Controllers/APIController.cs
+++ VNASTWebsite/Controllers/APIController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,28 +13,62 @@ namespace VNASTWebsite.Controllers
 {
     public class APIController : Controller
     {
+        private string serverUrl = "http://23.97.243.166:3000/";
+        private string userToken;
+
         // GET: API
         public ActionResult Index()
         {
             return View();
         }
 
-        public string LoginRequest()
+        // POST: login request to API server
+        public bool LoginRequest(string username, string password)
         {
             HttpWebRequest webRequest = (HttpWebRequest)WebRequest
-                .Create("http://23.97.243.166:3000/login");
-            webRequest.KeepAlive = false;
+                .Create(serverUrl + "login");
             webRequest.Method = "POST";
-
             webRequest.ContentType = "application/json; charset=UTF-8";
             webRequest.Accept = "application/json";
             using (var streamWriter = new StreamWriter(webRequest.GetRequestStream()))
             {
-                string json = " { 'username': 'admin', 'password': 'admin' } ";
-                streamWriter.Write(json);
-                streamWriter.Flush();
+                string requestBody = string.Format("{{\"username\":\"{0}\",\"password\":\"{1}\"}}", username, password);
+                streamWriter.Write(requestBody);
                 streamWriter.Close();
             }
+            
+            string jsonResponse = "";
+            using (Stream s = webRequest.GetResponse().GetResponseStream())
+            {
+                using (StreamReader sr = new StreamReader(s))
+                {
+                    jsonResponse = sr.ReadToEnd();
+                }
+            }
+
+            JObject response = JObject.Parse(jsonResponse);
+            try
+            {
+                userToken = (string)response["token"]; 
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                return false;
+            }
+
+            return true;
+        }
+
+        // GET: generic get method for API, parameter is page URI 
+        public string RequestGet(string page)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest
+                .Create(serverUrl + page);
+            webRequest.Method = "GET";
+            webRequest.Accept = "application/json";
+            webRequest.Headers.Add("x-access-token", userToken);
+
             string jsonResponse = "";
             using (Stream s = webRequest.GetResponse().GetResponseStream())
             {
